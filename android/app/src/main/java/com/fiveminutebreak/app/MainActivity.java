af77c9af2b441fdf41ce06c09a42a9b184938d8b
bskcorona-github <kanemasa.tatsuro@gmail.com>
2025-05-15 01:26:41 +0900

    初期コミット：5分休憩タイマーアプリ

---
+++ android/app/src/main/java/com/fiveminutebreak/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fiveminutebreak.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fiveminutebreak.app',
+  appName: '5分休憩タイマー',
+  webDir: 'www'
+};
+
+export default config;
